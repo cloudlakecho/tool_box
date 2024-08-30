93b14eac33544439ab8e7abda22f777ad108acec
Cloud Cho <cloudlakecho@users.noreply.github.com>
2024-08-30 10:45:38 -0700

    Update test.sql

--- test.sql
+++ test.sql
@@ -95,3 +95,33 @@ SELECT name
     WHERE referee_id IS NULL
     OR 
     NOT referee_id = 2
+
+/*
+Exmple 4
+Employee =
+| empId | name   | supervisor | salary |
+| ----- | ------ | ---------- | ------ |
+| 3     | Brad   | null       | 4000   |
+| 1     | John   | 3          | 1000   |
+| 2     | Dan    | 3          | 2000   |
+| 4     | Thomas | 3          | 4000   |
+Bonus =
+| empId | bonus |
+| ----- | ----- |
+| 2     | 500   |
+| 4     | 2000  |
+Output
+| name | bonus |
+| ---- | ----- |
+| Brad | null  |
+| John | null  |
+| Dan  | 500   |
+*/ 
+
+SELECT name, bonus
+  FROM Employee
+    LEFT JOIN Bonus
+      ON Employee.empID = Bonus.empID
+  WHERE bonus < 1000
+  OR
+  bonus IS NULL
